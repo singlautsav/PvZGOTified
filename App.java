0f43cbec21768eb9d29a4bb35fdf7ba53ce730bb
Utsav <utsav18321@iiitd.ac.in>
2019-10-21 23:45:57 +0530

    Added Module File Project Configured

--- App.java
+++ App.java
@@ -1,4 +1,7 @@
 package PvZGOTified;
 
 public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
 }
---
+++ src/module-info.java
@@ -0,0 +1,7 @@
+module pvz {
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires javafx.graphics;
+
+    opens sample;
+}
